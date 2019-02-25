39cd4300a6b58e84ce378a53a270575c91fb0637
Loren Frates <cubedbits@outlook.com>
2019-02-24 22:33:09 -0800

    Moved climbing systems to their own class

--- src/main/java/frc/team3238/robot/FREDDX.java
+++ src/main/java/frc/team3238/robot/FREDDX.java
@@ -1,9 +1,5 @@
 package frc.team3238.robot;
 
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.FollowerType;
-import com.ctre.phoenix.motorcontrol.InvertType;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -11,6 +7,7 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.team3238.robot.control.CameraController;
 import frc.team3238.robot.control.FREDDXControlScheme;
 import frc.team3238.robot.control.SwitchableControls;
+import frc.team3238.robot.systems.Climber;
 import frc.team3238.robot.systems.Manipulator;
 import frc.team3238.robot.systems.PodDrive;
 
@@ -26,9 +23,7 @@ public final class FREDDX extends TimedRobot {
     private FREDDXControlScheme robotController;
     private DifferentialDrive   drive;
     private Manipulator         manipulator;
-    private WPI_TalonSRX        spudsTalon;
-    private WPI_TalonSRX        rollerTalon;
-    private WPI_TalonSRX        breacherMasterTalon;
+    private Climber             climber;
 
     private FREDDX() {
 
@@ -36,55 +31,16 @@ public final class FREDDX extends TimedRobot {
 
     @Override
     public void robotInit() {
-        //Initialize controls
+        //Initialize user controls
         driveJoystick       = new Joystick(DRIVE_JOYSTICK_PORT);
         manipulatorJoystick = new Joystick(MANIPULATOR_JOYSTICK_PORT);
 
-        //Initialize talons
-        WPI_TalonSRX breacherSlaveTalon = new WPI_TalonSRX(BREACHER_LEFT_ID);
-        spudsTalon          = new WPI_TalonSRX(SPUDS_ID);
-        rollerTalon         = new WPI_TalonSRX(ROLLER_ID);
-        breacherMasterTalon = new WPI_TalonSRX(BREACHER_RIGHT_ID);
-
-        //Apply talon reversals
-        spudsTalon.setInverted(REVERSE_SPUDS);
-        rollerTalon.setInverted(REVERSE_ROLLER);
-        breacherMasterTalon.setInverted(REVERSE_BREACHER);
-        breacherSlaveTalon.setInverted(REVERSE_BREACHER);
-
-        //Configure talon brake state
-        spudsTalon.setNeutralMode(SPUDS_BRAKE ? NeutralMode.Brake : NeutralMode.Coast);
-        rollerTalon.setNeutralMode(ROLLER_BRAKE ? NeutralMode.Brake : NeutralMode.Coast);
-        breacherMasterTalon.setNeutralMode(BREACHER_BRAKE ? NeutralMode.Brake : NeutralMode.Coast);
-        breacherSlaveTalon.setNeutralMode(BREACHER_BRAKE ? NeutralMode.Brake : NeutralMode.Coast);
-
-        //Pair up talons
-        breacherSlaveTalon.follow(breacherMasterTalon, FollowerType.PercentOutput);
-        breacherSlaveTalon.setInverted(InvertType.OpposeMaster);
-
-        //Configure feedback sensors
-        spudsTalon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, TALON_TIMEOUT);
-        breacherMasterTalon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, TALON_TIMEOUT);
-
-        //Set sensor phase (used to make sensor negative when motion is negative)
-        spudsTalon.setSensorPhase(FLIP_SPUD_SENSOR);
-        breacherMasterTalon.setSensorPhase(FLIP_BREACHER_SENSOR);
-
-        //Putting in PID constants
-        spudsTalon.config_kP(0, SPUDS_kP, TALON_TIMEOUT);
-        spudsTalon.config_kI(0, SPUDS_kI, TALON_TIMEOUT);
-        spudsTalon.config_kD(0, SPUDS_kD, TALON_TIMEOUT);
-        breacherMasterTalon.config_kP(0, BREACHER_kP, TALON_TIMEOUT);
-        breacherMasterTalon.config_kI(0, BREACHER_kI, TALON_TIMEOUT);
-        breacherMasterTalon.config_kD(0, BREACHER_kD, TALON_TIMEOUT);
-
-        //Initialize drive
+        //Initialize systems
         drive = new PodDrive(DRIVE_LEFT_MASTER_ID, DRIVE_LEFT_SLAVE_ID, DRIVE_RIGHT_MASTER_ID, DRIVE_RIGHT_SLAVE_ID);
         drive.setDeadband(0);
         drive.setSafetyEnabled(true);
-
-        //Initialize manipulator
         manipulator = new Manipulator();
+        climber     = new Climber();
 
         //Initialize controllers
         cameraController = new CameraController(manipulatorJoystick);
@@ -126,15 +82,15 @@ public final class FREDDX extends TimedRobot {
     }
 
     public WPI_TalonSRX getBreacher() {
-        return breacherMasterTalon;
+        return climber.breacherMaster;
     }
 
     public WPI_TalonSRX getSpuds() {
-        return spudsTalon;
+        return climber.spuds;
     }
 
     public WPI_TalonSRX getRoller() {
-        return rollerTalon;
+        return climber.roller;
     }
 
     public WPI_TalonSRX getLift() {
---
+++ src/main/java/frc/team3238/robot/systems/Climber.java
@@ -0,0 +1,56 @@
+package frc.team3238.robot.systems;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.FollowerType;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import static frc.team3238.robot.FREDDXConstants.*;
+
+public class Climber {
+
+    //Yes they are not encapsulated on purpose
+    public final WPI_TalonSRX roller;
+    public final WPI_TalonSRX spuds;
+    public final WPI_TalonSRX breacherMaster;
+
+    public Climber(WPI_TalonSRX roller, WPI_TalonSRX spuds, WPI_TalonSRX breacherLeft, WPI_TalonSRX breacherRight) {
+        this.roller         = roller;
+        this.spuds          = spuds;
+        this.breacherMaster = breacherRight;
+
+        roller.setInverted(REVERSE_ROLLER);
+        roller.setNeutralMode(ROLLER_BRAKE ? NeutralMode.Brake : NeutralMode.Coast);
+
+        spuds.setInverted(REVERSE_SPUDS);
+        spuds.setNeutralMode(SPUDS_BRAKE ? NeutralMode.Brake : NeutralMode.Coast);
+        spuds.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, TALON_TIMEOUT);
+        spuds.setSensorPhase(FLIP_SPUD_SENSOR);
+        spuds.config_kP(0, SPUDS_kP, TALON_TIMEOUT);
+        spuds.config_kI(0, SPUDS_kI, TALON_TIMEOUT);
+        spuds.config_kD(0, SPUDS_kD, TALON_TIMEOUT);
+
+        breacherRight.setInverted(REVERSE_BREACHER);
+        breacherRight.setNeutralMode(BREACHER_BRAKE ? NeutralMode.Brake : NeutralMode.Coast);
+        breacherRight.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, TALON_TIMEOUT);
+        breacherRight.setSensorPhase(FLIP_BREACHER_SENSOR);
+        breacherRight.config_kP(0, BREACHER_kP, TALON_TIMEOUT);
+        breacherRight.config_kI(0, BREACHER_kI, TALON_TIMEOUT);
+        breacherRight.config_kD(0, BREACHER_kD, TALON_TIMEOUT);
+
+        breacherLeft.setInverted(REVERSE_BREACHER);
+        breacherLeft.setNeutralMode(BREACHER_BRAKE ? NeutralMode.Brake : NeutralMode.Coast);
+        breacherLeft.follow(breacherRight, FollowerType.PercentOutput);
+        breacherLeft.setInverted(InvertType.OpposeMaster);
+    }
+
+    public Climber(int rollerTalonId, int spudsTalonId, int breacherLeftTalonId, int breacherRightTalonId) {
+        this(new WPI_TalonSRX(rollerTalonId), new WPI_TalonSRX(spudsTalonId), new WPI_TalonSRX(breacherLeftTalonId),
+             new WPI_TalonSRX(breacherRightTalonId));
+    }
+
+    public Climber() {
+        this(ROLLER_ID, SPUDS_ID, BREACHER_LEFT_ID, BREACHER_RIGHT_ID);
+    }
+}
